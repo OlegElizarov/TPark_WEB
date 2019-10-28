0cb77caf47c9b96f5744dde674605ecde22b2c27
Oleg <elizarov1999@list.ru>
2019-10-28 21:06:47 +0300

    Login+logout+paginator_base

--- mysite/ask/static/js/buttons.js
+++ mysite/ask/static/js/buttons.js
@@ -1,8 +1,6 @@
- function myFunction1() {
-        var el=document.getElementsByClassName("btn-group-vertical");
-        el=el.getAttribute("id");
-          document.getElementById(el).stepUp(1);
-      }
-      function myFunction2(str) {
-          document.getElementById(str).stepDown(1);
-      }
+function myFunction1(str) {
+    document.getElementById(str).stepUp(1);
+}
+function myFunction2(str) {
+    document.getElementById(str).stepDown(1);
+}
