a2547a62176065480966e967c5d4f52cdc075b7d
Oleg <elizarov1999@list.ru>
2019-11-03 13:51:20 +0300

    Author model_step1

--- mysite/ask/models.py
+++ mysite/ask/models.py
@@ -4,10 +4,27 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 from django.db.models import Count
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 #Question.objects.annotate(num_tags=Count('tags',distinct=True)).order_by('-num_tags')
 #Tag.objects.annotate(num_tags=Count('question')).order_by('-num_tags')
+#author = models.ForeignKey(
+#        User, on_delete=models.SET_NULL, null=True, blank=True)
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birth_date = models.DateField(null=True, blank=True)
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Author.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
 
 
 class TagManager(models.Manager):
